0115bc9f3e165b01ccb4c99f672d1096d131cb8b
Brendon Ghaderi <3301521+brendong@users.noreply.github.com>
2026-03-06 14:16:33 -0800

    Initial formula, README, and auto-update workflow

---
+++ Formula/olane.rb
@@ -0,0 +1,18 @@
+class Olane < Formula
+  desc "oLane Network CLI - multi-agent architecture toolkit"
+  homepage "https://github.com/olane-labs/o-cli"
+  url "https://registry.npmjs.org/@olane/o-cli/-/o-cli-0.7.7.tgz"
+  sha256 "c8d3eae160a892e32837db3dcae515e843e5383fef52b8141940c8bcf8b6d59f"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/olane --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,24 @@
+# oLane Homebrew Tap
+
+Homebrew formulae for [oLane](https://github.com/olane-labs/o-cli) tools.
+
+## Installation
+
+```bash
+brew tap olane-labs/tap
+brew install olane
+```
+
+## Upgrade
+
+```bash
+brew update
+brew upgrade olane
+```
+
+## Uninstall
+
+```bash
+brew uninstall olane
+brew untap olane-labs/tap
+```
